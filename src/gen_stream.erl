487606b7b3db0e9282d25dc300f57291107b4f58
Jay Nelson <jay@duomark.com>
2012-03-31 20:41:57 -0700

    Change copyright to DuoMark

--- src/gen_stream.erl
+++ src/gen_stream.erl
@@ -1,13 +1,13 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010. All Rights Reserved.
+%% Copyright DuoMark International, Inc.  2010-2012. All Rights Reserved.
 %%
-%% The contents of this file are subject to the Erlang Public License,
-%% Version 1.1, (the "License"); you may not use this file except in
-%% compliance with the License. You should have received a copy of the
-%% Erlang Public License along with this software. If not, it can be
-%% retrieved online at http://www.erlang.org/.
+%% The contents of this file are subject to the New BSD license,
+%% you may not use this file except in compliance with the License.
+%% You should have received a copy of the License along with this
+%% software. If not, it can be retrieved online at
+%%  http://www.github.com/duomark/gen_stream/LICENSE.
 %%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
